0ed252d282a0d32d57799694b9c7fb0b27ae7177
Stephen J. Carnam <steve@steveorevo.com>
2015-06-17 12:24:26 -0700

    Initial checkin.
    Signed-off-by: Stephen J. Carnam <steve@steveorevo.com>

---
+++ innodb-autoconvert.php
@@ -0,0 +1,9 @@
+<?php
+/**
+Plugin Name: InnoDB Autoconvert
+Plugin URL: https://serverpress.com/plugins/innodb-autoconvert
+Description: Convert a site's tables to InnoDB on Create, Copy, Move, and Import operations.
+Version: 1.0.0
+Author: Stephen Carnam
+Author URI: http://steveorevo.com
+ */
