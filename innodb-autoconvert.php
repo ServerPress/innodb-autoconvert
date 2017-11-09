0f53e5f89781c789f86040fb27575b7575f58af9
Dave Jesch <dave@davejesch.com>
2017-11-09 12:09:45 -0800

    update version in preparation for release #2

--- innodb-autoconvert.php
+++ innodb-autoconvert.php
@@ -3,7 +3,7 @@
 Plugin Name: InnoDB Autoconvert
 Plugin URL: https://serverpress.com/plugins/innodb-autoconvert
 Description: Convert a site's tables to InnoDB on Create, Copy, Move, and Import operations.
-Version: 1.0.0
+Version: 1.0.1
 Author: Stephen J. Carnam
 Author URI: http://steveorevo.com
  */
